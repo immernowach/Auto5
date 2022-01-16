b1724476e6eac2fa1548717a17a2df1dc5bff5c2
schoggiph <77555097+schoggiph@users.noreply.github.com>
2022-01-16 18:31:22 +0100

    initial

---
+++ src/main/java/com/example/application/views/list/DataController.java
@@ -0,0 +1,14 @@
+package com.example.application.views.list;
+
+public class DataController {
+    private int ButtonSelection = 1;
+
+    public int getButtonSelection() {
+        return ButtonSelection;
+    }
+
+    public void setButtonSelection(int buttonSelection) {
+        ButtonSelection = buttonSelection;
+        System.out.println(ButtonSelection);
+    }
+}
