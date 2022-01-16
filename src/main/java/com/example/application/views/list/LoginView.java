398eea589c1d0752d0cc9ce28b7bda62d033ad81
herzkadani <71609839+herzkadani@users.noreply.github.com>
2022-01-16 22:26:10 +0100

    error message

--- src/main/java/com/example/application/views/list/LoginView.java
+++ src/main/java/com/example/application/views/list/LoginView.java
@@ -5,12 +5,15 @@ import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Login | Login")
 @Route(value = "login", layout = MainLayout.class)
-public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+public class LoginView extends VerticalLayout implements HasUrlParameter<String> {
 
     private final LoginForm login = new LoginForm();
 
@@ -20,19 +23,16 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        login.setAction("login");
+        login.setAction("login/error");
 
         add(new H1("Philipp's Garage"), login);
     }
 
-    @Override
-    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) { //TODO Fehlermeldung erstellen
-        // inform the user about an authentication error
-        if(beforeEnterEvent.getLocation()
-                .getQueryParameters()
-                .getParameters()
-                .containsKey("error")) {
-            login.setError(true);
-        }
-    }
+    
+
+	@Override
+	public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
+		if(parameter != null && parameter.equals("error"))
+			login.setError(true);
+	}
 }
